dba566cd2021232fd036225c523526654517207f
Thomas <12407096+thomas-chauvet@users.noreply.github.com>
2019-02-20 13:25:31 +0100

    Update install_tools.sh

--- install_tools.sh
+++ install_tools.sh
@@ -12,7 +12,7 @@ echo "Install environment_kernels"
 pip install environment_kernels
 jupyter notebook --generate-config
 echo "c.NotebookApp.kernel_spec_manager_class = 'environment_kernels.EnvironmentKernelSpecManager'
-c.EnvironmentKernelSpecManager.conda_env_dirs=['~/anaconda/envs/']
+c.EnvironmentKernelSpecManager.conda_env_dirs=['~/anaconda3/envs/']
 c.EnvironmentKernelSpecManager.find_virtualenv_envs=False
 c.EnvironmentKernelSpecManager.display_name_template='~Env ({})'" >> ~/.jupyter/jupyter_notebook_config.py
 conda install nb_conda
