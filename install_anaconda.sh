9f236fca7a822ffab1b70f3e99edd47bb4037ec4
Thomas <12407096+thomas-chauvet@users.noreply.github.com>
2019-02-20 10:30:20 +0100

    fix typo

--- install_anaconda.sh
+++ install_anaconda.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cd /tmp/
-curl -O https://repo.continuum.io/archive/Anaconda3-5.3.0-Linux-x86_64.sh.sh
+curl -O https://repo.continuum.io/archive/Anaconda3-5.3.0-Linux-x86_64.sh
 bash Anaconda3-5.1.0-Linux-x86_64.sh
 source ~/.bashrc
